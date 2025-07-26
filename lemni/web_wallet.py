6b094f0995727ba285333cde35977836cceefc65
tjenkins47 <30880820+tjenkins47@users.noreply.github.com>
2025-07-26 04:40:10 -0400

    Add files via upload

--- lemni/web_wallet.py
+++ lemni/web_wallet.py
@@ -137,5 +137,7 @@ def wallet_view(wallet_id):
     )
 
 # === Run App ===
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
